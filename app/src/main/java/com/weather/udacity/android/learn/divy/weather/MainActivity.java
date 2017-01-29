32228b31667dfe846889cfef1f08582a5f645ad2
divyaPrakash <divs4debu@gmail.com>
2017-01-29 21:00:52 +0530

    Added ListView with dummy data - my version

--- app/src/main/java/com/weather/udacity/android/learn/divy/weather/MainActivity.java
+++ app/src/main/java/com/weather/udacity/android/learn/divy/weather/MainActivity.java
@@ -1,11 +1,14 @@
 package com.weather.udacity.android.learn.divy.weather;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +23,16 @@ public class MainActivity extends AppCompatActivity {
                     }
     }
     public  static class PlaceholderFragment extends Fragment{
-        public PlaceholderFragment(){}
+        public PlaceholderFragment(){
+        }
 
         public View onCreateView (LayoutInflater inflater, ViewGroup container,
                                   Bundle savedInstanceState){
             View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+            String[] days = {"Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"};
+            ArrayAdapter adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item,days);
+            ListView listView = (ListView) rootView.findViewById(R.id.list_view_forcast);
+            listView.setAdapter(adapter);
             return rootView;
         }
     }
